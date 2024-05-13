d315553ced7f3ac9e396adfd64c23bf1a782d255
Jin Ce <jince_hm@163.com>
2024-05-13 22:56:29 +0800

    自动导入依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import VueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {resolve} from "path";
+import {RouteRecordRaw} from "vue-router";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [
+        vue(),
+        VueDevTools(),
+        AutoImport({
+            imports: [
+                "vue",
+                "vue-router"
+            ],
+            dts: true
+        }),
+        Components({ /* options */})
+    ],
+    server: {
+        port: 80,
+        host: '0.0.0.0',
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, './src')
+        }
+    }
 })
